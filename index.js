71f97f48cc243dc13d413759a7bbe9ebf0ba002e
joaquimserafim <joaquim.serafim@gmail.com>
2014-05-28 00:51:45 +0100

    first release

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function convert (val) {
+  if (/^[-+]?(?:\d+(?:\.\d*)?|\.\d+)(e[-+]?\d+)?$/.test(val)) return Number(val);
+  if (/^(true|false)$/.test(val)) return Boolean(val);
+  if (/(\d{4}-\d{2}-\d{1,2}).*/.test(val)) return new Date(val);
+  if (/null/.test(val)) return null;
+  if (/undefined/.test(val)) return undefined;
+
+  return val;
+}
+
+
+module.exports = parser;
+
+function parser (args) {
+  args = args || process.argv.slice(2);
+  var key;
+  var obj = {_: []};
+
+  for (var i = 0; i < args.length; i++) {
+    if (args[i].indexOf('--') === 0) {
+      key = args[i].replace('--', '');
+
+      // let pass args this way --app=80
+      if (/=/.test(key)) {
+        var splArg = key.split('=');
+        obj[splArg[0]] = convert(splArg[1]);
+        key = null;
+      }
+
+      continue;
+    }
+
+    if (key) {
+      obj[key] = convert(args[i]);
+      key = null;
+    } else {
+      obj._.push(convert(args[i]));
+    }
+  }
+  return obj;
+}
---
+++ test.js
@@ -0,0 +1,48 @@
+var test = require('tape');
+var parser = require('./');
+
+
+test('', function (t) {
+  t.plan(6);
+
+  t.deepEqual(parser(['--port', '80']),
+              { _: [], port: 80 },
+              'parse a number - port: 80');
+
+  t.deepEqual(parser(['hello', 'world', '556.7']),
+              { _: ['hello', 'world', 556.7]},
+              'parse an array of args - [\'hello\', \'world\', 556.7]');
+
+  t.deepEqual(parser(['--date', '2014-05-29T12:00:02']),
+              { _: [], date: new Date('2014-05-29T12:00:02') },
+              'parse a date - date: Thu May 29 2014 13:00:02 GMT+0100 (WEST)');
+
+  t.deepEqual(parser(['--null_arg', 'null']),
+              { _: [], null_arg: null},
+              'parse a null value - null_arg: null');
+
+  t.deepEqual(parser(['--undefined_arg', 'undefined']),
+              { _: [], undefined_arg: undefined},
+              'parse a undefined value - undefined_arg: undefined');
+
+  t.deepEqual(parser(['--port', '80',
+                     'test1', 'test2',
+                     '--app=hello',
+                     '--status=true',
+                     '--clean=null',
+                     '--a', '122.2',
+                     '--b', 'undefined',
+                     '--c', '2012-09-11T11:00:00',
+                     '122.5']),
+              { _: [ 'test1', 'test2', 122.5 ],
+                a: 122.2,
+                app: 'hello',
+                b: undefined,
+                c: new Date('2012-09-11T11:00:00'),
+                clean: null,
+                port: 80,
+                status: true },
+              'parsing a big arguments line - ' +
+             '--port 80 test1 test2 --app=hello --status=true --clean=null' +
+             ' --a 122.2 --b undefined --c "2012-09-11T11:00:00" 122.5');
+});
